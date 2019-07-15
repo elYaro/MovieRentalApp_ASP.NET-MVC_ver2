2c19fe917892b2fd135848107d4d16450ef3ddd2
Jaroslaw Nowakowski <nowakowski.priv@gmail.com>
2019-07-15 15:53:42 +0200

    Customer Model passed to view chcanged from List to IEnumerable

--- MovieRentalApp_ASP.NET MVC_ver2/Controllers/CustomersController.cs
+++ MovieRentalApp_ASP.NET MVC_ver2/Controllers/CustomersController.cs
@@ -12,20 +12,35 @@ namespace MovieRentalApp_ASP.NET_MVC_ver2.Controllers
         // GET: Customers
         public ActionResult Index()
         {
-            var customers = new List<Customer>()
+            /*var customers1 = new List<Customer>()
+            {
+               
+            };
+            */
+            var customers = GetAllCustomers();
+            return View(customers);
+        }
+
+            // var customers = new 
+
+
+        private IEnumerable<Customer> GetAllCustomers()
+        {
+            return new List<Customer>
             {
                 new Customer {Id = 1, Name = "Bolek"},
                 new Customer {Id = 2, Name = "Lolek"},
                 new Customer {Id = 3, Name = "Tola"},
                 new Customer {Id = 4, Name = "Uszatek"}
             };
-
-
-
-
-
-            // var customers = new 
-            return View(customers);
         }
+
+
+
+
+
+
+            
+
     }
 }
