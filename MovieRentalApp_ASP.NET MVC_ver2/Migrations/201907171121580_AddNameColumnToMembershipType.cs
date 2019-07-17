b59ee5a65c69cb4a97666043737dc38d27ca710a
Jaroslaw Nowakowski <nowakowski.priv@gmail.com>
2019-07-17 14:18:12 +0200

    MembershipType Name added Details View updated in Name property Index View updated from Discount Rate into Membership Name

---
+++ MovieRentalApp_ASP.NET MVC_ver2/Migrations/201907171121580_AddNameColumnToMembershipType.cs
@@ -0,0 +1,18 @@
+namespace MovieRentalApp_ASP.NET_MVC_ver2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddNameColumnToMembershipType : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.MembershipTypes", "Name", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.MembershipTypes", "Name");
+        }
+    }
+}
---
+++ MovieRentalApp_ASP.NET MVC_ver2/Migrations/201907171131146_PopulatingMembershipTypeNamesInDataBase1.cs
@@ -0,0 +1,16 @@
+namespace MovieRentalApp_ASP.NET_MVC_ver2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulatingMembershipTypeNamesInDataBase1 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
