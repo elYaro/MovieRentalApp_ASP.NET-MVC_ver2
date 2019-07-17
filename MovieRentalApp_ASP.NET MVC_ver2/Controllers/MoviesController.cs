cd19ccfed38fbbac3e991bb386fb0c07a5926dbe
Jaroslaw Nowakowski <nowakowski.priv@gmail.com>
2019-07-17 22:38:02 +0200

    Db Context added in MovieController, Movie Index View updated, Movie Details View updated

--- MovieRentalApp_ASP.NET MVC_ver2/Controllers/MoviesController.cs
+++ MovieRentalApp_ASP.NET MVC_ver2/Controllers/MoviesController.cs
@@ -5,35 +5,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace MovieRentalApp_ASP.NET_MVC_ver2.Controllers
 {
     public class MoviesController : Controller
     {
-        /*
-        // GET: Movies/Random
-        public ActionResult Random()
-        {
-            var movie = new Movie() { Id = 1, Name = "Shrek!" };
-            var customers = new List<Customer>
-            {
-                new Customer { Id = 1, Name = "Bolek" },
-                new Customer { Id = 2, Name = "Lolek" },
-                new Customer { Id = 3, Name = "Tola"}
-            };
-
-            var ViewModel = new RandomMovieViewModel
-            {
-                Movie = movie,
-                Customers = customers
-            };
-
-            return View(ViewModel);
-        }
-        */
 
         /*
-
         // GET: Movies/Released/{year}/{month}
         [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1,12)}")]
         public ActionResult ByReleaseDate(int year, int month)
@@ -42,27 +21,33 @@ namespace MovieRentalApp_ASP.NET_MVC_ver2.Controllers
         }
         */
 
+
+
+        private MyDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new MyDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();    
+        }
+
+
         // GET: Movies/
         public ActionResult Index(int? id)
         {
-            var movies = GetAllMovies();
+            var movies = _context.Movies.Include(c => c.Genre).ToList();
             return View(movies);
         }
 
-        private IEnumerable<Movie> GetAllMovies()
-        {
-            return new List<Movie>
-                {
-                    new Movie { Id = 1, Name = "Shrek" },
-                    new Movie { Id = 2, Name = "Shrek2" },
-                    new Movie { Id = 3, Name = "Wall-e" },
-            };
-        }
         
         //GET : Movies/Details/{id}
         public ActionResult Details(int id)
         {
-            var movie = GetAllMovies().SingleOrDefault(m => m.Id == id);
+            var movie = _context.Movies.Include(c=>c.Genre).SingleOrDefault(m => m.Id == id);
             if (movie == null)
             {
                 return HttpNotFound();
