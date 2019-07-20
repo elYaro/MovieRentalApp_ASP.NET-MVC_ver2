d058c906c5feec9ccc303af519f517058ba1a92d
Jaroslaw Nowakowski <nowakowski.priv@gmail.com>
2019-07-20 18:05:56 +0200

    Save method in CustomerController implemented with logic: if to add a new customer or...  if to updat existing customer. Hidden field with customer.Id property added in CustomerForm

--- MovieRentalApp_ASP.NET MVC_ver2/Controllers/CustomersController.cs
+++ MovieRentalApp_ASP.NET MVC_ver2/Controllers/CustomersController.cs
@@ -60,14 +60,45 @@ namespace MovieRentalApp_ASP.NET_MVC_ver2.Controllers
 
 
         [HttpPost]
-        public ActionResult Create(Customer customer)
+        public ActionResult Save(Customer customer)
         {
-            _context.Customers.Add(customer);
+            if (customer.Id == 0)
+            {
+                _context.Customers.Add(customer);
+            }
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+
+
+                /*
+                TryUpdateModel(customerInDb);
+                properties in customerInDb object will be updated based on the key-value pairs in request data. 
+                This is an official Microsoft aproach suggested in ASP.NET tutorials on their page.
+                I will not use this approach because of the following issues:
+                1) it opens up security holes in the application: if we don't want the user to be able to update all properties in the Form but the User will be smart hacker may add additional key-value pairs in form data and with the method Hacker will update all properties.
+
+                Microsoft proposed the solution which is using the 3rd argument with the specified properties to be updated ex : TryUpdateModel(customerInDb, "", new string[] {"Name", "Email"});
+                 In this approach the problem is the magic strings. 
+
+                So will will ue another way to updated properties...
+                 */
+
+                customerInDb.Name = customer.Name;
+                customerInDb.BirthDate = customer.BirthDate;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter; 
+            }
+
             _context.SaveChanges();
 
             return RedirectToAction("Index", "Customers");
         }
         
+
+
+
+
         public ActionResult Edit(int id)
         {
             var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
