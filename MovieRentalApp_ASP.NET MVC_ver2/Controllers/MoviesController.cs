4c717fe196f81904b949bae49908dc804940796b
Jaroslaw Nowakowski <nowakowski.priv@gmail.com>
2019-07-22 15:00:39 +0200

    Save method in MovieController updated: works for new movies and edited movies now

--- MovieRentalApp_ASP.NET MVC_ver2/Controllers/MoviesController.cs
+++ MovieRentalApp_ASP.NET MVC_ver2/Controllers/MoviesController.cs
@@ -74,11 +74,34 @@ namespace MovieRentalApp_ASP.NET_MVC_ver2.Controllers
         }
 
         [HttpPost]
-        public ActionResult Create(Movie movie)
+        public ActionResult Save(Movie movie)
         {
-            movie.DateAdded = DateTime.Now;
+            if (movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
 
-            _context.Movies.Add(movie);
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieIdDb = _context.Movies.Single(m => m.Id == movie.Id);
+                /*
+                TryUpdateModel(movieInDb);
+                properties in movieInDb object will be updated based on the key-value pairs in request data. 
+                This is an official Microsoft aproach suggested in ASP.NET tutorials on their page.
+                I will not use this approach because of the following issues:
+                1) it opens up security holes in the application: if we don't want the user to be able to update all properties in the Form but the User will be smart hacker may add additional key-value pairs in form data and with the method Hacker will update all properties.
+
+                Microsoft proposed the solution which is using the 3rd argument with the specified properties to be updated ex : TryUpdateModel(movieInDb, "", new string[] {"Name", "Email"});
+                 In this approach the problem is the magic strings. 
+
+                So will will ue another way to updated properties...
+                 */
+                movieIdDb.Name = movie.Name;
+                movieIdDb.NumberInStock = movie.NumberInStock;
+                movieIdDb.ReleaseDate = movie.ReleaseDate;
+                movieIdDb.GenreId = movie.GenreId;
+            }
 
             _context.SaveChanges();
 
