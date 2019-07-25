e56bb740699b621d4f746e96d2412465b844627c
Jaroslaw Nowakowski <nowakowski.priv@gmail.com>
2019-07-25 14:17:30 +0200

    Refactoring Megic Numbers in Min18YearsIfMember class

--- MovieRentalApp_ASP.NET MVC_ver2/Models/Min18YearsIfAMember.cs
+++ MovieRentalApp_ASP.NET MVC_ver2/Models/Min18YearsIfAMember.cs
@@ -12,7 +12,8 @@ namespace MovieRentalApp_ASP.NET_MVC_ver2.Models
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
             var customer = (Customer)validationContext.ObjectInstance;
-            if (customer.MembershipTypeId == 0|| customer.MembershipTypeId == 1)
+            if (customer.MembershipTypeId == MembershipType.Unknown ||
+                customer.MembershipTypeId == MembershipType.PayAsYouGo)
             {
                 return ValidationResult.Success;
             }
