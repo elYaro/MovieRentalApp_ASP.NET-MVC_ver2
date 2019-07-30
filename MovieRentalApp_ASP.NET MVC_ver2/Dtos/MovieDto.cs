38177422de11d00e0b089dd65aab7438a56c47f9
Jaroslaw Nowakowski <nowakowski.priv@gmail.com>
2019-07-30 16:37:55 +0200

    Dto class  for Movies added

---
+++ MovieRentalApp_ASP.NET MVC_ver2/Dtos/MovieDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MovieRentalApp_ASP.NET_MVC_ver2.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter movie's name")]  //OVERRIDING the default error message
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        [Range(1, 20)]
+        public short NumberInStock { get; set; }
+
+        [Required]
+        public byte GenreId { get; set; }
+    }
+}
