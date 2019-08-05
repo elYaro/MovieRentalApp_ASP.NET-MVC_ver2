0eaaf7fa84f51cafad8b84408f57347c46ec716c
Jaroslaw Nowakowski <nowakowski.priv@gmail.com>
2019-08-05 16:11:53 +0200

    with Package Manager installed bootbox package, refference to bootbox.js added in bundleConfig.cs, dialogbox in Index.cshtml switched to bootstrap to look nicer

--- MovieRentalApp_ASP.NET MVC_ver2/App_Start/BundleConfig.cs
+++ MovieRentalApp_ASP.NET MVC_ver2/App_Start/BundleConfig.cs
@@ -20,7 +20,8 @@ namespace MovieRentalApp_ASP.NET_MVC_ver2
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootbox.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
