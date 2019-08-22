68c948e68ffa923be13d74d2fdeeaad847a4be32
Jaroslaw Nowakowski <nowakowski.priv@gmail.com>
2019-08-22 13:47:05 +0200

    random movie view rendered

---
+++ MovieRentalApp_ASP.NET_MVC_ver2.1/Controllers/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿using MovieRentalApp_ASP.NET_MVC_ver2._1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieRentalApp_ASP.NET_MVC_ver2._1.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek" };
+
+
+            return View(movie);
+        }
+    }
+}
